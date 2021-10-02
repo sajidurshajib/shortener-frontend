c33be13bf43a1490ca21757dc2f55d6fb71d8772
sajidurshajib <sajidur.inbox@gmail.com>
2021-10-03 00:01:59 +0600

    profile component creted

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,11 @@
+import classes from './Profile.module.css'
+
+const Profile = () => {
+    return (
+        <div className={classes.Profile}>
+            <h2>Profile</h2>
+        </div>
+    )
+}
+
+export default Profile
