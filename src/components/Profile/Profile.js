cc10b7f08b9fd6f737a5eeb3a5ae9968aa9230c7
sajidurshajib <sajidur.inbox@gmail.com>
2021-10-03 23:38:49 +0600

    little bit css

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -3,7 +3,11 @@ import classes from './Profile.module.css'
 const Profile = () => {
     return (
         <div className={classes.Profile}>
-            <h2>Profile</h2>
+            <div className={classes.character}>
+                <h2>
+                    Demo Guy<span> - demo@gmail.com</span>
+                </h2>
+            </div>
         </div>
     )
 }
